89eba7dac100d8ae066b948d493b2884f4f3aa2e
ishjuke <ishanbijukuchhay@gmail.com>
2026-07-10 20:10:51 -0400

    Fix twitter-image runtime export

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,3 +1,4 @@
 // Reuse the Open Graph image as the Twitter/X card image.
 // Next.js picks this up automatically from app/twitter-image.tsx.
-export { default, runtime, alt, size, contentType } from "./opengraph-image";
+export const runtime = "edge";
+export { default, alt, size, contentType } from "./opengraph-image";
